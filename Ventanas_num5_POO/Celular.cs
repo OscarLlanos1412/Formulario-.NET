407e3d78361ffa76abe5affdbef8026881ed71ae
OscarLlanos1412 <ojllanos25@misena.edu.co>
2021-10-08 12:19:36 -0500

    Inicio de formularios con POO

---
+++ Ventanas_num5_POO/Celular.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos
+{
+    class Celular
+    {
+        public string Marca;
+        public string Color;
+        public string Tipo;
+
+        public Celular()
+        {
+            Marca = "Ipro";
+            Color = "Gris";
+            Tipo = "Tactil";
+        }
+
+        public Celular(string M)
+        {
+            Marca = M;
+            Color = "Gris";
+            Tipo = "Tactil";
+        }
+
+        public Celular(string M, string C, string T)
+        {
+            Marca = M;
+            Color = C;
+            Tipo = T;
+        }
+
+        public void Llamar()
+        {
+            System.Windows.Forms.MessageBox.Show("Marca: " + Marca + " Color: " + Color + " Tipo: " + Tipo);
+        }
+
+        public string MandarMensaje(string N, string Msj)
+        {
+            string Mensaje = Msj;
+            string Nombre = N;
+
+            string Enviar = "De: " + Nombre + " Mensaje: " + Mensaje;
+            return Enviar;
+        }
+    }
+}
---
+++ Ventanas_num5_POO/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Productos; //Otra forma de poder acceder a un namespace
+
+namespace Ventanas_num5_POO
+{
+    public partial class Form1 : Form
+    {
+        //Productos.Celular iphone = new Productos.Celular(); //Seria una forma de llamar 
+        //O crear un objeto de diferente namespace
+
+        Celular iphone = new Celular();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_llamar_Click(object sender, EventArgs e)
+        {
+            //iphone.Llamar();
+            MessageBox.Show(iphone.MandarMensaje("Oscar", "Eh como jue patirusia"));
+        }
+    }
+}
